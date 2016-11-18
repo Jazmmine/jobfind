cb67e5245604c463996ebcd3d21aded9601564d0
Jazmine Angoma <jazmine.angoma@gmail.com>
2016-11-17 21:20:32 -0500

    subiendo data

--- public/js/app.js
+++ public/js/app.js
@@ -6,10 +6,7 @@ function initMap() {
   });
 }
 
-var clientId = "581403453733193";
-
-var template = '<div class="contenedor-informacion">' +
-        '<div class="row">' + 
+var template =  '<div class="row">' + 
             '<div class="col s12">' +
                 '<span class="trabajo-general">{{trabajogeneral}}</span>' +
                 '<span class="lugar-general">{{lugargeneral}}</span>' +
@@ -29,23 +26,36 @@ var template = '<div class="contenedor-informacion">' +
             '<div class="col s12">' +
                 '<div class="requerimiento-deltrabajo">{{requerimientodeltrabajo}}</div>' +
             '</div>' +
-        '</div>' +
-    '</div>'
-
-var pedirMuestra = function(response){
-    console.log(response);
-    var informacion = "";
-
-}
+        '</div>' 
 
 $(document).ready(function(){
-    var indeedApi = "http://api.indeed.com/ads/apisearch?publisher=581403453733193&q=java&l=austin%2C+tx&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co = nosotros y CHNL = & userip = 1.2.3.4 y useragent = Mozilla / 2F4.0%%% 29 28Firefox & v = 2";
-    var inputCiudad = $("#ciudad").val();
-    var inputTrabajo = $("#trabajo").val();
-    $("#btn-resultado").click(function(e){
-        e.preventDefault();
-        
+    $("#btn-resultado").click(function(){
+        var indeedApiUrl = "http://api.indeed.com/ads/apisearch";
+        var inputCiudadValor = $("#ciudad").val();
+        var inputTrabajoValor = $("#trabajo").val();
+        var data = {
+            publisher: "8023780673544955",
+            format: "json",
+            q: inputTrabajoValor,
+            l: inputCiudadValor,
+            limit: "10",
+            v:"2"
+        }
+
+        $.ajax({
+            url: indeedApiUrl,
+            type: 'GET',
+            data: data,
+            dataType: 'jsonp',
+            success: function(response){
+                console.log(response);
+                $("#informacion-indeed").html(template.replace("{{trabajogeneral}}",response.results[0].city))
+
+            }
+
+        })
     });
-    $.getJSON(indeedApi, )
+    
+
 
 });
